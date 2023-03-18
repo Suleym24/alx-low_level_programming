96a68376e3ec4c92fbb67b98f08eda4806e80f8a
Suleym24 <suleymodedare@gmail.com>
2023-03-17 19:02:42 -0700

    7-print_tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - reverse alphabet
+ * Return: always 0
+ */
+
+int  main(void)
+
+{
+	char rev;
+
+	for (rev = 'z'; rev >= 'a'; rev++)
+	putchar(rev);
+	putchar('\n');
+	return (0);
+}
