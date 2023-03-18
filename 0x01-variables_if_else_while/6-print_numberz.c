ede6595e9d51a6e1344da4ebd640e5a49a4be580
Suleym24 <suleymodedare@gmail.com>
2023-03-17 18:50:33 -0700

    6-print_numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints numbers
+ * Return: Return 0
+ */
+
+int main(void)
+
+{
+	int dig;
+
+	for (dig = 0; dig < 10; dig++)
+	putchar(dig);
+	putchar('\n');
+	return (0);
+}
