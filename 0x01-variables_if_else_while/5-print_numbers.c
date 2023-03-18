881544d8def79c16e5f7c5279a3adda50307cf8b
Suleym24 <suleymodedare@gmail.com>
2023-03-17 18:40:38 -0700

    5-print_numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -3,14 +3,14 @@
  * main - prints numbers
  * Return: always 0
  */
+
 int main(void)
+
 {
 	int dig;
 
 	for (dig = 0; dig < 10; dig++)
-	{
-		printf("%d", dig);
-		printf("\n");
-	}	
+	printf("%d", dig);
+	printf("\n");
 	return (0);
 }
