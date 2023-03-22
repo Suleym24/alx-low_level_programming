6ea54349eb4e8f6ed29d5594a8b61ac71270c027
Suleym24 <suleymodedare@gmail.com>
2023-03-21 17:56:25 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 -> prints the lowercase alphabets
+ */
+
+void print_alphabet_x10(void)
+{
+	int alph, i;
+
+	for (i = 0; i <= 10; i++)
+	{
+		for (alph = 'a'; alph <= 'z'; alph++)
+		{
+			_putchar(alph);
+		}
+	}
+	_putchar('\n');
+}
+
