9a8921774bfadd6af32424b8bcf10fae22165847
Suleym24 <suleymodedare@gmail.com>
2023-03-21 17:03:28 -0700

    Hot fix

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 /**
  * main -  entry point
  * Return: Always 0
@@ -6,7 +6,7 @@
 int main(void)
 
 {
-	_putchar('_');
+	_puthchar('_');
 	_putchar('p');
 	_putchar('u');
 	_putchar('t');
