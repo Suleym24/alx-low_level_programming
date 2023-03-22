402c7956b001132191f9ef0f558f537030d7367e
Suleym24 <suleymodedare@gmail.com>
2023-03-21 17:21:08 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - entry point
+ * Return: Always 0
+ */
+void print_alphabet(void)
+{
+	alph = 'a';
+	while (alph <= 'z')
+	{
+		_putchar(alph);
+	}
+	_putchar('\n');
+}
