8ddd08e69d9a6ded1ff28422f42d6a9479ada1ad
Suleym24 <suleymodedare@gmail.com>
2023-03-23 19:20:25 -0700

    104-fibonacci

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main -> entry point
+ * Return: always 0
+ */
+int main(void)
+{
+       long i, a = 1, b = 2, c;
+
+	for (i = 1; i <= 98; i++)
+	{
+		printf("%ld", a);
+		c = a + b;
+		a = b;
+		b = c;
+		if (i < 98)
+		printf(", ");
+	}
+	printf("\n");
+	return (0);
+}	
