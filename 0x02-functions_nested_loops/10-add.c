c51128271ebb82fff387f492fcc74aac45cbdfcc
Suleym24 <suleymodedare@gmail.com>
2023-03-23 17:23:01 -0700

    10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,10 +1,14 @@
 #include "main.h"
 /**
- *
- *
+ * add -> adds two integers
+ * @num1: a parameter
+ * @num2: a parameter
+ * Return: Returns calAdd
  */
 int add(int num1, int num2)
+
 {
 	int calAdd = num1 + num2;
+
 	return (calAdd);
 }
