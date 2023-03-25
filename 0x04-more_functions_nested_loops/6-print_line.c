2271ae6a7d3b5057c4807f0d600edeeac02fdf83
Suleym24 <suleymodedare@gmail.com>
2023-03-24 20:40:03 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_line -> prints a straight line
+ */
+void print_line(int n)
+{
+	int n, i;
+	for (i = 0; i <= n; i++)
+	{
+		if (n <= 0)
+		{
+			_putchar('\n');
+		}
+		else
+		{
+			_putchar('_');
+		}
+	}
+}
+
