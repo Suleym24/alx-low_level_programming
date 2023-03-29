a91549d117c6881d40bd64f6300f765c63477275
Suleym24 <suleymodedare@gmail.com>
2023-03-28 20:00:27 -0700

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * puts_half -> prints half of a string
+ * @str: a parameter
+ * Return: nothing
+ */
+void puts_half(char *str)
+{
+	int i = 0, j = 0, n;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		j = j + 1;
+	}
+	if (j % 2 != 0)
+	{
+		n = j - 1 / 2;
+	}
+	else
+	{
+		n = j / 2;
+	}
+	for (i = 0; i <= n; i++)
+	{
+		_putchar(s[i]);
+	}
+}
