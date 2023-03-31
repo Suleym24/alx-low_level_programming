3aa6e9a9419609d31de1f4bd2f1b6310c4f6db7f
Suleym24 <suleymodedare@gmail.com>
2023-03-30 17:41:06 -0700

    1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/*
+ * _strncat -> concatenates strings
+ * @dest: a parameter
+ * @src: a parameter
+ * @n: a parameter
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i = 0, delen = 0;
+
+	while (dest[i++])
+		delen++;
+
+	for (i = 0; src[i] && i < n; index++)
+		dest[delen++] = src[i];
+
+	return (dest);
+}
