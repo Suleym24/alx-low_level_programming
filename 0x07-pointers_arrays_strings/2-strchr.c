51c04d7fe9190d7a764ddb7fdc1a57f87bfc6206
Suleym24 <suleymodedare@gmail.com>
2023-04-10 17:58:19 -0700

    2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _strchr -> locates a character in a string
+ * @s: a parameter
+ * @c: a parameter
+ * Return: returns a string or null depending on the condition
+ */
+char *_strchr(char *s, char c)
+{
+	int a = 0, i;
+
+	while (s[a])
+		a++;
+	for (i = 0; i < b; i++)
+	{
+		if (c == s[i])
+			s += i;
+		return (s);
+	}
+	return ('\0');
+}
