54a399e90e69292f4686cb154ed28c7ed055550b
Suleym24 <suleymodedare@gmail.com>
2023-04-13 17:52:06 -0700

    100-set_string.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -9,12 +9,12 @@ void print_diagsums(int *a, int size)
 {
 	int i, j, sum1 = 0, sum2 = 0, maxsum = 0;
 
-	for (i = 0; *a[i][size]; i++)
+	for (i = 0; a[i][size]; i++)
 	{
 		for (j = size; j >= 0; j--)
 		{
-			sum1 = *a[i][i];
-			sum2 = *a[i][j];
+			sum1 = a[i][i];
+			sum2 = a[i][j];
 			maxsum = maxsum + sum1 + sum2;
 		}
 	}
