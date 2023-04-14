887a8a66822c05d62b30bf37421c75b271600501
Suleym24 <suleymodedare@gmail.com>
2023-04-13 17:17:36 -0700

    7-print_chessboard.c

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -2,3 +2,4 @@ int _putchar(char c);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
 char *_strpbrk(char *s, char *accept);
+void print_chessboard(char (*a)[8]);
