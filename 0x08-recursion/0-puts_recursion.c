689964ff45baa8762915e282cb735f029ac69baa
Suleym24 <suleymodedare@gmail.com>
2023-04-14 20:02:32 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _puts_recursion - prints a string
+ * @s: a parameter
+ */
+void _puts_recursion(char *s)
+{
+	int i;
+
+		for (i = 0; s[i] != '\0'; i++)
+			_putchar(s[i]);
+	_putchar('\n');
